06351ec994a48f89f1201c5cd65cf6e169045898
oshue <os2@shuebox.com>
2024-01-29 19:23:52 -0600

    complete 2 5b

---
+++ assign2_5b.py
@@ -0,0 +1,25 @@
+# Program 05 b
+# area of a circle pi*r**2 - input using float c = pi*D
+import math
+
+
+radius = float(input("Please enter the radius of the circle: "))
+
+area = math.pi * (radius)**2
+print("\nthe area of a circle with radius {}, is {:3.2f}".format(radius,area))
+
+
+
+
+
+
+
+def code():
+    
+
+    #diameter = circumference / math.pi
+    
+    #print("\nthe area of a circle of circumference {}, is {}".format(circumference,area))
+    #print("\nthe area of a circle of circumference {0}, is {1:3.2f}".format(circumference,area))
+    #print("\nthe area of a circle of circumference %3.2f is %3.2f" % (circumference,area))
+    pass
