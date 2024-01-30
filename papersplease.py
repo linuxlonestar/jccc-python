c00f4343bf4b48608566ffbf5fd14fed39d8916e
oshue <os2@shuebox.com>
2024-01-29 18:53:51 -0600

    Create papersplease.py

---
+++ papersplease.py
@@ -0,0 +1,20 @@
+import time
+
+print("papers, please\n")
+
+time.sleep(4)
+
+name = input("enter a valid name\n")
+
+if name == "god":
+    print("you shouldn't have come.\n\nend")
+    exit()
+    
+seal = input("enter a valid seal code.\n")
+
+if seal == "1234":
+    print("welcome!")
+
+else:
+    print("you are not allowed in.\n")
+    print("end")
