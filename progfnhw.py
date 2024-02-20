1b34de8aa384fbc7e68b98cabc95b88dcede01ca
owen <os2@shuebox.com>
2024-02-19 19:15:52 -0600

    Update progfnhw.py

--- progfnhw.py
+++ progfnhw.py
@@ -1,29 +1,41 @@
 # code to create a list of 10 values
 values =[]
 print(f"Please enter 10 values\n")
-for i in range(0,10):
-    item = input(f"Please enter item {i + 1}: ")
-    values.append(item) 
 
-print(values)
+def inputNumbers():
+    for i in range(0,10):
+        item = input(f"Please enter item {i + 1}: ")
+        values.append(item) 
+    print(values)
 
-print(f"Integer values are:")
-for item in values:
-    if item.isnumeric():
-        if "." not in item:
-            print(item)     
+inputNumbers()
 
-print(f"Float values are:")
-for item in values:
-    if "."  in item:
-        print(item)        
 
-print(f"text values are:")
-for item in values:
-    if not item.isnumeric():
-        if "." not in item:
-            print(item)
+def intPrint():
+    print(f"Integer values are:")
+    for item in values:
+        if item.isnumeric():
+            if "." not in item:
+                print(item)     
 
+intPrint()
+
+def floatPrint():
+    print(f"Float values are:")
+    for item in values:
+        if "."  in item:
+            print(item)        
+
+floatPrint()
+
+def textPrint():
+    print(f"text values are:")
+    for item in values:
+        if not item.isnumeric():
+            if "." not in item:
+                print(item)
+
+textPrint()
 
 if __name__=='__main__':
     pass
